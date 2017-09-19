a5bb3fddfc530b0cf2d22ce4d99d59c62661555c
Ben Nilsen <ben@pixomobile.com>
2017-09-19 02:07:28 -0700

    fix header logo sizing

--- colorpicker/src/components/ColorDetail.js
+++ colorpicker/src/components/ColorDetail.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ColorCard from './ColorCard';
 import { Link } from 'react-router-dom';
 
-
 function shadeColor(color, percent) {
   const f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
   return (0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
@@ -15,7 +14,7 @@ function renderShades(hex) {
     result.push(shadeColor('#' + hex, value));
   }
   return result.map( (color) => {
-    return  (
+    return (
       <div key={color} className="col-xs-6 col-sm-4 col-md-3">
         <ColorCard colorData={{hex: `${color}`}} />
       </div>
